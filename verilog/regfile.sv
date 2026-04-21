644f00ec1eff72c04edc431a07ab7e1be0f46207
ql2585 <ql2585@cadpc33.ee.columbia.edu>
2026-04-21 19:12:37 -0400

    2 way superscalar

--- verilog/regfile.sv
+++ verilog/regfile.sv
@@ -1,55 +1,51 @@
-/////////////////////////////////////////////////////////////////////////
-//                                                                     //
-//   Modulename :  regfile.sv                                          //
-//                                                                     //
-//  Description :  This module creates the Regfile used by the ID and  //
-//                 WB Stages of the Pipeline.                          //
-//                                                                     //
-/////////////////////////////////////////////////////////////////////////
-
-`include "verilog/sys_defs.svh"
-
-// P4 TODO: update this with the new parameters from sys_defs
+`include "sys_defs.svh"
 
 module regfile (
-    input             clock, // system clock
-    // note: no system reset, register values must be written before they can be read
-    input [4:0]       read_idx_1, read_idx_2, write_idx,
-    input             write_en,
-    input [`XLEN-1:0] write_data,
-
-    output logic [`XLEN-1:0] read_out_1, read_out_2
+    input             clock,
+    input [4:0]       read_idx_1,
+    input [4:0]       read_idx_2,
+    input [4:0]       read_idx_3,
+    input [4:0]       read_idx_4,
+    input             write_en_0,
+    input [4:0]       write_idx_0,
+    input [`XLEN-1:0] write_data_0,
+    input             write_en_1,
+    input [4:0]       write_idx_1,
+    input [`XLEN-1:0] write_data_1,
+    output logic [`XLEN-1:0] read_out_1,
+    output logic [`XLEN-1:0] read_out_2,
+    output logic [`XLEN-1:0] read_out_3,
+    output logic [`XLEN-1:0] read_out_4
 );
 
-    logic [31:1] [`XLEN-1:0] registers; // 31 XLEN-length Registers (0 is known)
+    logic [31:1] [`XLEN-1:0] registers;
 
-    // Read port 1
-    always_comb begin
-        if (read_idx_1 == `ZERO_REG) begin
-            read_out_1 = 0;
-        end else if (write_en && (write_idx == read_idx_1)) begin
-            read_out_1 = write_data; // internal forwarding
-        end else begin
-            read_out_1 = registers[read_idx_1];
+    function automatic [`XLEN-1:0] rf_read(
+        input [4:0] idx
+    );
+        begin
+            if (idx == `ZERO_REG)
+                rf_read = '0;
+            else if (write_en_1 && (write_idx_1 == idx) && (write_idx_1 != `ZERO_REG))
+                rf_read = write_data_1;
+            else if (write_en_0 && (write_idx_0 == idx) && (write_idx_0 != `ZERO_REG))
+                rf_read = write_data_0;
+            else
+                rf_read = registers[idx];
         end
+    endfunction
+
+    always_comb begin
+        read_out_1 = rf_read(read_idx_1);
+        read_out_2 = rf_read(read_idx_2);
+        read_out_3 = rf_read(read_idx_3);
+        read_out_4 = rf_read(read_idx_4);
     end
 
-    // Read port 2
-    always_comb begin
-        if (read_idx_2 == `ZERO_REG) begin
-            read_out_2 = 0;
-        end else if (write_en && (write_idx == read_idx_2)) begin
-            read_out_2 = write_data; // internal forwarding
-        end else begin
-            read_out_2 = registers[read_idx_2];
-        end
-    end
-
-    // Write port
     always_ff @(posedge clock) begin
-        if (write_en && write_idx != `ZERO_REG) begin
-            registers[write_idx] <= write_data;
-        end
+        if (write_en_0 && (write_idx_0 != `ZERO_REG))
+            registers[write_idx_0] <= write_data_0;
+        if (write_en_1 && (write_idx_1 != `ZERO_REG))
+            registers[write_idx_1] <= write_data_1;
     end
-
-endmodule // regfile
+endmodule
